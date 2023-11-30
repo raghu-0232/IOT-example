7d86d3c635a4854c6a2330cbabc6b287d77f4ddf
raghu-0232 <58412332+raghu-0232@users.noreply.github.com>
2023-11-30 23:20:26 +0530

    Update GetPut.py

--- prod/GetPut.py
+++ prod/GetPut.py
@@ -1,16 +1,10 @@
 import requests
-import csv
 from datetime import datetime
 import time
 import mysql.connector
 
 # MySQL database configuration
-db_config = {
-    'host': 'localhost',     # e.g., 'localhost'
-    'user': 'root',     # e.g., 'raghu'
-    'password': '123456',
-    'database': 'esp8266',  # e.g., 'your_esp8266_database'
-}
+db_config = {'host': 'localhost', 'user': 'root', 'password': '123456', 'database': 'esp8266'}
 
 # ESP8266 IP address
 esp_ip = "192.168.0.193"
@@ -19,74 +13,38 @@ esp_ip = "192.168.0.193"
 temperature_uri = f"http://{esp_ip}/temperature"
 humidity_uri = f"http://{esp_ip}/humidity"
 
-# CSV file name
-csv_file = "temperature_humidity_data.csv"
-
 def get_sensor_data():
     try:
-        # Make HTTP GET requests to ESP8266
-        temperature_response = requests.get(temperature_uri)
-        humidity_response = requests.get(humidity_uri)
-
-        # Return temperature and humidity as a tuple
-        return temperature_response.text, humidity_response.text
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+        return requests.get(temperature_uri).text, requests.get(humidity_uri).text
+    except requests.exceptions.ConnectionError:
+        print("Connection error. Retrying...")
         return None, None
 
-def save_to_csv(data):
-    # Get current timestamp
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # Append data to CSV file
-    with open(csv_file, mode='a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([timestamp, *data])
-
 def insert_into_database(temperature, humidity):
-    # Connect to MySQL
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor()
 
     try:
-        # SQL query to insert data into the table
-        query = "INSERT INTO sensor_data (temperature, humidity) VALUES (%s, %s)"
-        data = (float(temperature) if temperature else None, float(humidity) if humidity else None)
-
-        # Execute the query
-        cursor.execute(query, data)
-
-        # Commit changes
+        cursor.execute("INSERT INTO sensor_data (temperature, humidity) VALUES (%s, %s)",
+                       (float(temperature), float(humidity)))
         connection.commit()
         print("Data inserted into MySQL database.")
     except Exception as e:
         print(f"Error: {e}")
-        # Rollback in case of an error
         connection.rollback()
     finally:
-        # Close the cursor and connection
         cursor.close()
         connection.close()
 
 if __name__ == "__main__":
     try:
         while True:
-            # Get sensor data
             temperature, humidity = get_sensor_data()
 
-            # Print the data (optional)
             if temperature is not None and humidity is not None:
                 print(f"Temperature: {temperature} °C, Humidity: {humidity} %")
-            else:
-                print("Error: Failed to get sensor data.")
+                insert_into_database(temperature, humidity)
 
-            # Save data to CSV file
-            save_to_csv([temperature, humidity])
-
-            # Insert data into MySQL database
-            insert_into_database(temperature, humidity)
-
-            # Wait for 10 seconds before the next reading
             time.sleep(10)
     except KeyboardInterrupt:
         print("Script terminated by user.")
