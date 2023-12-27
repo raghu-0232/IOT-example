7ecf4647929ae5a77f559749f0547122a0ba9c44
raghu-0232 <58412332+raghu-0232@users.noreply.github.com>
2023-12-27 23:47:56 +0530

    Update church.ino

--- boards/church.ino
+++ boards/church.ino
@@ -1,28 +1,103 @@
-int pins[] = {5, 4, 0, 2}; // Assign GPIO pins 0, 4, 2, and 5 on NodeMCU
-int currentPin = 0; // Keeps track of the current pin
-int delays[] = {1500, 1000, 500}; // Delay times for each state (in milliseconds)
-int currentDelayIndex = 0; // Keeps track of the current delay index
+#include <M5Atom.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <DHT.h>
+#include "secrets/Secrets.h"
+
+#define DHTPIN 25  // M5Atom uses GPIO 25 for DHT
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+float t = 0.0;
+float h = 0.0;
+
+const long interval = 10000;
+unsigned long previousMillis = 0;
 
 void setup() {
-  // Initialize GPIO pins as OUTPUT
-  for (int i = 0; i < 4; i++) {
-    pinMode(pins[i], OUTPUT);
+  M5.begin();
+  Serial.begin(115200);
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
   }
+
+  Serial.println(WiFi.localIP());
+
+  client.setServer(mqttBroker, mqttPort);
 }
 
 void loop() {
-  int currentDelay = delays[currentDelayIndex];
+  M5.update();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    float newT = dht.readTemperature();
+    if (!isnan(newT)) {
+      t = newT;
+    }
 
-  // Turn on the current pin
-  digitalWrite(pins[currentPin], HIGH);
+    float newH = dht.readHumidity();
+    if (!isnan(newH)) {
+      h = newH;
+    }
 
-  // Delay for the specified time
-  delay(currentDelay);
+    if (!client.connected()) {
+      reconnect();
+    }
 
-  // Turn off the current pin
-  digitalWrite(pins[currentPin], LOW);
+    if (t == 0.0 && h == 0.0) {
+      if (publishToMQTT("test", "Sensor not connected or error occurred", 1)) {
+        Serial.println("Error message published to MQTT");
+      } else {
+        Serial.println("Failed to publish error message to MQTT. Retrying in 5 seconds...");
+      }
+    } else {
+      String payload = "{\"temperature\":" + String(t) + ",\"humidity\":" + String(h) + "}";
+      if (publishToMQTT("test", payload, 1)) {
+        Serial.println("Data published to MQTT");
+      } else {
+        Serial.println("Failed to publish data to MQTT. Retrying in 5 seconds...");
+      }
+    }
 
-  // Increment the current pin index and delay index
-  currentPin = (currentPin + 1) % 4;
-  currentDelayIndex = (currentDelayIndex + 1) % 3;
+    delay(1000);
+  }
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT broker...");
+    if (client.connect("M5AtomClient")) {
+      Serial.println("Connected to MQTT broker");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" Retrying in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+bool publishToMQTT(const char *topic, const String &payload, int qos) {
+  // Convert String payload to a character array.
+  const char* charPayload = payload.c_str();
+
+  // Publish with the correct parameters - topic, payload, and QoS
+  if (client.publish(topic, charPayload, qos)) {
+    return true;
+  } else {
+    Serial.print("Failed to publish to topic ");
+    Serial.print(topic);
+    Serial.println(". Retrying...");
+    return false;
+  }
 }
