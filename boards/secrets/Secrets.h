dafb126efc98f28dbc3aaf3b6a8b5b433ba251c2
raghu-0232 <58412332+raghu-0232@users.noreply.github.com>
2023-12-27 15:09:39 +0530

    Delete boards/secrets directory

--- boards/secrets/Secrets.h
+++
@@ -1,9 +0,0 @@
-// Secrets.h
-
-// Wi-Fi credentials
-const char *ssid = "Ravi";
-const char *password = "7093081041";
-const char *mqttBroker = "65.0.144.52";  // Replace with your MQTT broker address
-const int mqttPort = 1883;  // Default MQTT port
-const char *mqttUser = "raghu";  // Replace with your MQTT username
-const char *mqttPassword = "123456";  // Replace with your MQTT password
