bfd68d2bd863dec90a1724bc2b7cc8757a1c2828
raghu-0232 <58412332+raghu-0232@users.noreply.github.com>
2024-01-11 01:00:47 +0530

    Update Secrets.h

--- Setup-Device/secrets/Secrets.h
+++ Setup-Device/secrets/Secrets.h
@@ -1,18 +1,16 @@
 // secrets.h
 
 // WiFi Credentials
-const char* ssid = "ssid"; // Replace with your WiFi SSID
-const char* password = "password"; // Replace with your WiFi password
+const char* ssid = "Ravi"; // Replace with your WiFi SSID
+const char* password = "7093081041"; // Replace with your WiFi password
 
 // MQTT Broker Settings
-const char* mqtt_server = "mqtt_server.com"; // Replace with your MQTT broker address
+const char* mqtt_server = "devopsonsensors.com"; // Replace with your MQTT broker address
 const int mqtt_port = 8883; // MQTT SSL port, usually 8883
 const char* mqttTopic = "test";
 // SSL Certificate for the MQTT broker
 const char* ca_cert = \
 "-----BEGIN CERTIFICATE-----\n" \
 "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
-"TzELazCCA1OgAwIBAgIRAIIQz7DSQONZIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
-"cmNoIazCCA1OgAwIBAgIRAIIQz7DSQONIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
-"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
 "-----END CERTIFICATE-----\n";
